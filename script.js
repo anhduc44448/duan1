af51ce4dfd28920242cb5ede04f70e0b3a6d8f4a
duc anh <anhduc44448@gmail.com>
2025-09-29 11:35:50 +0700

    sua anh 5

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function drawBoard(board) {
       const piece = board[row][col];
       if (piece !== "--") {
         const img = document.createElement("img");
-        const imgSrc = `/chess/static/images/${pieceMap[piece] || piece}.png`;
+        const imgSrc = `./static/images/${pieceMap[piece] || piece}.png`;
         img.src = imgSrc;
         img.alt = piece;
         img.onload = () => console.log("Tải ảnh thành công:", imgSrc);
